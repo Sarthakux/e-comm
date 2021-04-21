aace3c877e9221ab01018ea7cb03ec73932a40e6
Aditya Pratap Singh Hada <49001649+apshada@users.noreply.github.com>
2021-04-21 20:03:14 +0530

    Update ShowCart.jsx

--- src/ShowCart.jsx
+++ src/ShowCart.jsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import { useContext } from 'react';
-import { BrowserRouter as  Link } from 'react-router-dom'  ;
+import React, { useState, useEffect } from "react";
+import { useContext } from "react";
+import { BrowserRouter as Link } from "react-router-dom";
 import { NotesContext } from "./context/context";
-function ShowCart () {
-    const { Cart } = useContext(NotesContext);
-      return(
-            
-            <div>
-            <Link to ="/">Back Dashboard</Link>
-            {Cart.map((product) => {
-                return(
-                    <div>
-                    <img src ={product.url} alt={product.name} key ={product.key}/>
-                    <div>{product.name}</div>
-                    <div>{product.price}</div>
-                    </div>
-                    );
-                 
-            })}
-            </div>
-            
-    
-);   
+function ShowCart(props) {
+  // const { Cart } = useContext(NotesContext);
+  const { state } = props.location;
+  console.log(state, "Cart Main");
+
+  const [Cart, setCart] = useState([]);
+
+  useEffect(() => {
+    setCart(state);
+  }, []);
+
+  const removeFromCart = (el) => {
+    let hardCopy = [...Cart];
+    hardCopy = hardCopy.filter((cartItem) => cartItem.productid !== el);
+    setCart(hardCopy);
+  };
+
+  return (
+    <div>
+      <Link to="/">Back Dashboard</Link>
+      {Cart?.map((product) => {
+        return (
+          <div key={product.productid}>
+            <img src={product.productUrl} alt={product.productName} />
+            <div>{product.name}</div>
+            <div>{product.productPrice}</div>
+            <button
+              onClick={() => {
+                removeFromCart(product.productid);
+              }}
+            >
+              Remove
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
-export default ShowCart;;
+export default ShowCart;
